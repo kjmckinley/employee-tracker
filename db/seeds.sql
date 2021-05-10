1933792cf66213cc2c99bc1fa04cf2906bbf4b7d
Kyle <kj.mckinley@gmail.com>
2021-05-09 22:06:49 -0500

    updates

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+-- Employee Tracker
+
 USE employee_db;
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Kyle", "McLinley")
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Kyle", "McLinley", 1);
