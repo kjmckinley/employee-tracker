b7fe09818c5b938561f9847cbeff96d204f6f292
Kyle <kj.mckinley@gmail.com>
2021-07-07 23:15:21 -0500

    added function that selects the employee that the user wishes to update

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ const startMenu = () => {
     })
 }
 
-// Functions to view all user options
+// Functions to VIEW all user options
 
 // function view all departments
 const viewDepartments = () => {
@@ -129,7 +129,7 @@ ORDER BY employees.id;`
     })
 }
 
-// functions for all add options
+// functions for all ADD options
 
 // function to add a department to the database
 const addDept = () => {
@@ -248,3 +248,88 @@ db.query(sql, (err, res) => {
     })
 })
 }  
+
+// a function that inserts user data on new employee into the database
+const completeAddEmployee = (newEmployeeData) => {
+    const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) 
+    VALUES (?,?,?,?)`;
+    const params = [newEmployeeData.first_name, newEmployeeData.last_name, newEmployeeData.role_id, newEmployeeData.manager_id]
+  
+    db.query(sql, params, (err, result) => {
+      if (err){
+        console.log(err)
+      }
+      console.log(`New ${newEmployeeData.title}, ${newEmployeeData.first_name} ${newEmployeeData.last_name}, added successfully.`)
+      return startMenu();
+    })
+}
+
+// function to select a manager using the data on employees in the database
+const selectManager = (newEmployeeData) => {
+
+    const sql = `SELECT employees.id, employees.first_name, employees.last_name, roles.title FROM employees JOIN roles ON employees.role_id = roles.id WHERE roles.title LIKE '%Manager%' OR roles.title LIKE '%Director%'`;
+  
+    db.query(sql, (err, res) => {
+      for (let i = 0; i < res.length; i++) {
+        manager = `${res[i].first_name} ${res[i].last_name}`, `${res[i].title}`
+        managerArr.push(manager)
+      }
+      inquirer.prompt({
+        type: 'list',
+        name: 'manager',
+        message: 'Select manager',
+        choices: managerArr.map(manager => `${manager}`)
+      }).then(manager => {
+        newEmployeeData.manager = manager.manager
+  
+        let index = manager.manager.indexOf(" ")
+        newEmployeeData.manager_first_name = manager.manager.substr(0, index)
+        newEmployeeData.manager_last_name = manager.manager.substr(index + 1)
+        const sql = `SELECT id FROM employees WHERE first_name = ? AND last_name = ?`;
+        const params = [newEmployeeData.manager_first_name, newEmployeeData.manager_last_name]
+        db.query(sql, params, (req, res) => {
+          newEmployeeData.manager_id = res[0].id
+          completeAddEmployee(newEmployeeData)
+        })
+      })
+    })
+  }
+
+// functions for all UPDATE options
+
+// empty current employee to insert into
+let currentEmployee = {}
+
+// function that allows user to select the employee they would like to update
+const selectEmployee = () => {
+  employeeArr = []
+
+  const sql = `SELECT * FROM employees`;
+  db.query(sql, (err, res) => {
+    if (err) throw err
+    for (let i = 0; i < res.length; i++) {
+      let employee = `${res[i].first_name} ${res[i].last_name}`
+      employeeArr.push(employee)
+    }
+    inquirer.prompt({
+      type: 'list',
+      name: 'updateEmployee',
+      message: 'Which employee would you like to update?',
+      choices: employeeArr.map(employee => `${employee}`)
+
+    }).then(employee => {
+      let index = employee.updateEmployee.indexOf(" ")
+      currentEmployee.first_name = employee.updateEmployee.substr(0, index)
+      currentEmployee.last_name = employee.updateEmployee.substr(index + 1)
+
+      const sql = `SELECT id FROM employees WHERE first_name = ? AND last_name = ?`;
+      const params = [currentEmployee.first_name, currentEmployee.last_name]
+
+      db.query(sql, params, (err, res) => {
+        if(err) throw err;
+        currentEmployee.id = res[0].id
+        chooseRole(currentEmployee)
+      })
+    })
+  })
+}
