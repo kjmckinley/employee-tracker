0752e94d9aadc8e3378092cd7e1a7d97ebd37ca5
Kyle <kj.mckinley@gmail.com>
2021-07-07 22:41:25 -0500

    added viewEmployees function to server

--- server.js
+++ server.js
@@ -110,4 +110,18 @@ const viewDepartments = () => {
     })
   }
 
-  
+  const viewEmployees= () => {
+
+    const sql= `SELECT employees.id, employees.first_name, employees.last_name, employees.manager_id, roles.title, roles.salary, departments.dept_name 
+    FROM employees 
+    JOIN roles ON employees.role_id = roles.id 
+    JOIN departments ON roles.dept_id = departments.id 
+    ORDER BY employees.id;`
+    
+    
+      db.query(sql, (err, res) => {
+        if (err) throw err
+        console.table(res)
+        startMenu();
+      })
+    }
